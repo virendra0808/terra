89dffa48c8f4e08bad1be668c2249c5aace2a8a0
virendra0808 <virendra0808@gmail.com>
2022-12-14 20:16:14 +0000

    done

--- backend.tf
+++
@@ -1,9 +0,0 @@
-# --- root/backend.tf ---
-
-terraform {
-  backend "s3" {
-    bucket = "week-24-project-100"
-    key    = "remote.tfstate"
-    region = "us-east-1"
-  }
-}
--- networking/variables.tf
+++
@@ -1,9 +0,0 @@
-# --- networking/variables.tf ---
-
-variable "vpc_cidr" {
-  type = string
-}
-
-variable "public_cidrs" {
-  type = list(any)
-}
